733911f9c34ef78a0bc6b8eab095f7e7697245e6
SingaLeCapi <mauricesinga@outlook.com>
2025-02-10 01:25:28 +0100

    Basic of Arrays in Go

--- composite_types/go_arrays.go
+++ composite_types/go_arrays.go
@@ -2,5 +2,13 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("Hi There the training begins here!!!")
+	nums := [3]int{1, 3, 5}
+	modifyArray(&nums)	
+	fmt.Println("New value of nums:", nums )
 }
+
+// Modifying the value of an array using a pointer
+func modifyArray(arr *[3]int) {
+	arr[0] = 11;
+}
+
