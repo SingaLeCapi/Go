a97b8a37fcaef91844ffb2c9e3106056bf874520
SingaLeCapi <mauricesinga@outlook.com>
2025-02-13 20:53:23 +0100

    Solved reversing array elements using go

--- exercises/pointer_array_reverse.go
+++ exercises/pointer_array_reverse.go
@@ -2,6 +2,13 @@ package exercises
 
 import "fmt"
 
-func ReverseArrayUsingPointers(){
-	fmt.Println("Hello")
+func ReverseArrayUsingPointers(arr [20]int ) [20]int {
+	left, right := 0, len(arr) - 1
+	for left < right {
+		arr[left], arr[right] = arr[right], arr[left]
+		left++
+		right--
+	}
+	fmt.Println("Reversed array: ", arr)
+	return arr
 }
